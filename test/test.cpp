17b401fd333855162f31dada0c7cb6dfcd8fc5ca
ToyasDhake <toyasdhake@gmail.com>
2019-09-29 10:47:11 -0400

    Two test cases added

--- test/test.cpp
+++ test/test.cpp
@@ -4,5 +4,12 @@
 TEST(computeTest, should_pass) {
     PID pid(0,0,0,0,0,0);
     double something = pid.compute(0,0);
-    EXPECT_EQ(0, something);
+    EXPECT_EQ(1, something);
+}
+
+TEST(constructorTest, should_pass) {
+    int temp = 13 ;
+    PID pid(0,0,0,temp,0,0);
+    double something = pid.getSampleTime();
+    EXPECT_EQ(temp, something);
 }
