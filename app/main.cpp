261b5c8c8cf8e3960f51ccc7d61c571ae64c357e
NJNischal <njnischal@gmail.com>
2019-11-25 16:20:52 -0500

    Added dependencies to the cmakelists and added the new class includes to main file

--- app/main.cpp
+++ app/main.cpp
@@ -1,25 +1,40 @@
-//! "Copyright [2019] Jagadesh,ToyasDhake,ShivamAkhauri,ChinmayJoshi"
+/**
+*
+*Copyright (c) 2019 Nagireddi Jagadesh Nischal
+*
+*Redistribution and use in source and binary forms, with or without modification, are permitted *provided that the following conditions are met:
+*
+*1. Redistributions of source code must retain the above copyright notice, this list of conditions and *the following disclaimer.
+*
+*2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions *and the following disclaimer in the documentation and/or other materials provided with the *distribution.
+*
+*3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse *or promote products derived from this software without specific prior written permission.
+*
+*THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR *IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND *FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR *CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL *DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, *DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER *IN *CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT *OF THE *USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**/
+
 /**
 * @file main.cpp
-* @author Jagadesh NischalNagireddi,Toyas Dhake,ShivamAkhauri,ChinmayJoshi
-* @date 26 Spetember 2019
+* @author Jagadesh Nischal Nagireddi
+* @date 25 November 2019
 * @brief This is a the main class for the PID implementation project
 * PID controller implementation for mobile robot.
 */
 
 #include <iostream>
-#include "../include/PID.h"
+#include "PID.h"
+#include "parameters.h"
 
 /**
 * @brief Main compute function for PID Controller
-* @author Jagadesh Nagareddi, Toyas Dhake
 */
 int main() {
     double change;
-    // Contructor initializing value Kp, Kd, Ki, SampleTime, OutMax, OutMin
-    PID mobileRobot(1, 1, 1, 13, 20, 0);
+    PID mobileRobot;
+    Parameter comp;
+
     // Main compute function
-    change = mobileRobot.compute(0, 0);
+    change = comp.compute(1.0, 1.0, mobileRobot);
     std::cout << "Computed velocity is: " << change << std::endl;
     return 0;
 }
