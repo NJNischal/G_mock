7f0c2c346ae7704d235dbb850fdd8ac8229748a1
Chinj17 <chinmaynj17@gmail.com>
2019-11-24 23:04:05 -0500

    Updated PID.h for gmock

--- include/PID.h
+++ include/PID.h
@@ -1,11 +1,44 @@
-
 /**
-* @file PID.h
-* @author Jagadesh Nischal Nagireddi (Driver), Toyas Dhake (Navigator)
-* @date 26 Spetember 2019
-* @copyright 2019 Jagadesh Nischal Nagireddi, Toyas Dhake
-* @brief This is a class for a PID controller
-*/
+ * @file PID.h
+ * @brief This is a the main class for the PID implementation project
+ *        PID controller implementation for mobile robot.
+ *
+ * BSD 3-Clause License
+ *
+ * @copyright Copyright (c) Chinmay Joshi
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @author Jagadesh NischalNagireddi,Toyas Dhake,ShivamAkhauri,ChinmayJoshi
+ *
+ * @date 11-24-2019
+ */
 
 #ifndef INCLUDE_PID_H_
 #define INCLUDE_PID_H_
@@ -13,34 +46,30 @@
 #include <chrono>
 
 class PID {
- private:
-    double kp;
-    double ki;
-    double kd;
-    double sampleTime;
-    double outMax;
-    double outMin;
-    double lastInput;
-    double iPart;
-    std::chrono::system_clock::time_point start;
-
  public:
-    // compute method here
-    double compute(double actualVelocity, double targetSetpoint);
-    PID(double _Kp, double _Kd, double _Ki, unsigned _SampleTime,
-               double _outMax, double _outMin);
-    void setKp(double _Kp);
-    double getKp();
-    void setKi(double _Ki);
-    double getKi();
-    void setKd(double _Kd);
-    double getKd();
-    void setSampleTime(double _SampleTime);
-    double getSampleTime();
-    void setOutMax(double _OutMax);
-    double getOutMax();
-    void setOutMin(double _OutMin);
-    double getOutMin();
+  double kp = 0;
+  double ki = 0;
+  double kd = 0;
+  double sampleTime = 0;
+  double outMax = 0;
+  double outMin = 0;
+  double lastInput = 0;
+  double iPart = 0;
+  std::chrono::system_clock::time_point start;
+  PID();
+  virtual ~PID();
+  void setKp(double Kp);
+  double getKp();
+  void setKi(double Ki);
+  double getKi();
+  virtual void setKd(double Kd);
+  virtual double getKd();
+  void setSampleTime(double SampleTime);
+  double getSampleTime();
+  void setOutMax(double OutMax);
+  double getOutMax();
+  void setOutMin(double OutMin);
+  double getOutMin();
 };
 
 #endif  // INCLUDE_PID_H_
